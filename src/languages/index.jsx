216ba1cb4d274381cb6fdb7b470a0742c67708d6
Isaack Njama <isaacknjama@proton.me>
2024-03-07 22:24:13 +0300

    feat: add language files

---
+++ src/languages/index.jsx
@@ -0,0 +1,21 @@
+import en from './en.json';
+import es from './es.json';
+import sw from './sw.json';
+
+export const languages = [
+  {
+    key: 'en-US',
+    description: 'English',
+    translation: en,
+  },
+  {
+    key: 'es',
+    description: 'Espanol',
+    translation: es,
+  },
+  {
+    key: 'sw',
+    description: 'Swahili',
+    translation: sw,
+  },
+];
